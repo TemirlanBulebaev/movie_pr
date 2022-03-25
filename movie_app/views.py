acd5f2f29242cca3860748af3c2787250c8d9cdd
Temirlan <tbulebaev@bk.ru>
2022-03-25 21:11:02 +0300

    Представление

--- movie_app/views.py
+++ movie_app/views.py
@@ -8,3 +8,9 @@ def show_all_movie(request):
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies
     })
+
+def show_one_movie(request, id_movie:int):
+    movie = Movie.objects.get(id=id_movie)
+    return render(request, 'movie_app/all_movies.html', {
+        'movie': movie
+    })
