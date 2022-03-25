4e0949d0565462b980d3e1c10e47335e87cff2f9
Temirlan <tbulebaev@bk.ru>
2022-03-25 16:19:17 +0300

    Представление

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Movie
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_app/all_movies.html', {
+        'movies': movies
+    })
