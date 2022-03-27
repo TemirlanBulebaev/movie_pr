860d2a3e1dca6302ee662717d2623871b0462b98
Temirlan <tbulebaev@bk.ru>
2022-03-27 19:54:10 +0300

    слаги

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import Movie
@@ -9,8 +9,8 @@ def show_all_movie(request):
         'movies': movies
     })
 
-def show_one_movie(request, id_movie:int):
-    movie = Movie.objects.get(id=id_movie)
-    return render(request, 'movie_app/all_movies.html', {
+def show_one_movie(request, id_movie:str, slug_movie=None):
+    movie = get_object_or_404(Movie, slug=slug_movie)
+    return render(request, 'movie_app/one_movies.html', {
         'movie': movie
     })
